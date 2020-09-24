74f557dc86ca2971c48619f4f7faae93ee61fc7c
irmawati69 <71766201+irmawati69@users.noreply.github.com>
2020-09-24 09:00:33 +0700

    Update redir.js

--- redir.js
+++ redir.js
@@ -1,16 +1,16 @@
 var urlmain = document.URL;
-var ad = document.URL.split("/") [3];
-var key = document.URL.split("/") [4];
-var id  = document.URL.split("/") [5];
+var md = document.URL.split("/")[3];
+var key = document.URL.split("/")[4];
+var id = document.URL.split("/")[5];
 
-if (md =='dep'){
- var redir='http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item'+id+'.;
- }else{
- var redir = 'https://s.aliexpress.com/e/'+key+';
- }
- 
- seTimeout(
- function( ){
- window.location = redir
- },
- 3000);
+if (md == 'deep') {
+  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
+} else {
+  var redir = 'https://s.click.aliexpress.com/e/'+key;
+}
+
+setTimeout(
+  function(){
+    window.location = redir 
+  },
+3000);
